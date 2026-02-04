f9453cf7039f0e5c608c5e9b1180cc536f115233
Nate Romack <nateromack2004@gmail.com>
2026-02-04 16:35:39 -0500

    first attempt at cat

--- part2/concat.c
+++ part2/concat.c
@@ -1,8 +1,31 @@
 
-char* concat(arg1, arg2){
+void concat(arg1, arg2){
 
   FILE *fptr1 = fopen(arg1, "r");
 
-  while()
+  if(fptr1 == NULL){
+
+    printf("File is not able to be opened");
+
+  }
+
+  char *outstr;
+
+  while(fgets(outstr, 2048, fptr1) != EOF){
+
+    printf("%s", outstr);
+
+  }
+
+  FILE *fptr2 = fopen(arg2, "r");
+
+  while(fgets(outstr, 2048, fptr1) != EOF){
+
+    printf("%s", outstr);
+
+  }
+
+  return 0;
+  
 
 }

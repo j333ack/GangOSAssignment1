0f40f997894b5f3834bb01837f609b7f30fd7f71
Nate Romack <nateromack2004@gmail.com>
2026-02-11 00:29:20 -0500

    Improved the type command

--- part2/concat.c
+++ part2/concat.c
@@ -2,16 +2,18 @@
 
 void concat(char* arg1,char* arg2){
 
-  if(arg1[0] == '\0' || arg2[0] == '\0'){
-    char *msg = "type requires two arguments";
-    printf("%s" ,msg);
-    return;
-
-  }
 
   FILE *fptr1 = fopen(arg1, "r");
 
   char out[MAXLINE];
+
+  if(fptr1 == NULL){
+
+    char *msg = "Error when opening file one";
+    printf("%s", msg);
+    return;
+
+  }
 
   while(fgets(out, MAXLINE, fptr1) != NULL){
 
@@ -20,17 +22,25 @@ void concat(char* arg1,char* arg2){
   }
 
   fclose(fptr1);
+  
+  if(arg2[0] != '\0'){
+    FILE *fptr2 = fopen(arg2, "r");
 
-  FILE *fptr2 = fopen(arg2, "r");
+    if(fptr2 == NULL){
 
-  while(fgets(out, MAXLINE, fptr1) != NULL){
+      char *msg = "Error when opening file two";
+      printf("%s", msg);
+      return;
+    }
+
+   while(fgets(out, MAXLINE, fptr1) != NULL){
     
-  printf("%s", out);
+      printf("%s", out);
 
-  }
+    }
 
-  fclose(fptr2);
-
+    fclose(fptr2);
+  } 
 
   return;
 }

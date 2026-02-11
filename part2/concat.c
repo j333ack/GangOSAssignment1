af8f1744e4144199fe3c1c98bb748c343c3da3ac
Nate Romack <nateromack2004@gmail.com>
2026-02-10 19:06:45 -0500

    finished cat

--- part2/concat.c
+++ part2/concat.c
@@ -1,8 +1,28 @@
 
+#MAX_LINE 2048
+
 char* concat(arg1, arg2){
 
   FILE *fptr1 = fopen(arg1, "r");
 
-  while()
+  char out[MAX_LINE];
 
+  while(fgets(out, MAX_LINE, fptr1) != NULL){
+
+    printf("%s\n", out);
+
+  }
+
+  FILE *fptr2 = fopen(arg2, "r");
+
+  while(fgets(out, MAX_LINE, fptr1) != NULL){
+    
+  printf("%s\n", out);
+
+  }
+
+
+  return 0;
 }
+
+
